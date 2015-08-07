a4489b062b382a636349dbfaabca600ca763bb92
Nora Delaney <nora.delaney@gmail.com>
2015-08-07 10:16:11 -0400

    fib

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,4 +1,16 @@
-def fibonacci(num)
-  return num if num <=1
-  fibonacci(num - 1) + fibonacci(num - 2)
+def fib num
+  if num < 0
+    return "Negative numbers not allowed"
+  elsif num == 0
+    return 0
+  elsif num == 1
+    return 1
+  else
+    return fib(num - 1) + fib(num - 2)
+  end
 end
+
+puts fib(3)
+puts fib(4)
+puts fib(5)
+puts fib(6)
